bf9548eb468f453e4418866f6beb2be1ccd15835
Ashley Avila <ashley.avila@me.com>
2018-03-26 21:49:24 -0700

    fixed warnings and updated buttons

--- src/components/SaveButtons.js
+++ src/components/SaveButtons.js
@@ -2,14 +2,30 @@ import React, { Component } from 'react';
 import { Button } from 'react-bootstrap'
 
 class SaveButtons extends Component {
+  constructor()
+  {
+    super();
+
+    this.importGrid = this.importGrid.bind(this);
+    this.exportGrid = this.exportGrid.bind(this);
+  }
+
+  importGrid() {
+    console.log("Import");
+  }
+
+  exportGrid() {
+    console.log("Export");
+  }
+
   render () {
     return (
       <div className="App-header">
-        <Button id="magicButton" bsStyle="default" bsSize="large">
-          Print
-        </Button>
-        <Button id="magicButton" bsStyle="default" bsSize="large">
+        <Button id="magicButton" bsStyle="default" bsSize="large" onClick={ this.importGrid }>
           Import
+        </Button>
+        <Button id="magicButton" bsStyle="default" bsSize="large" onClick={ this.exportGrid }>
+          Export
         </Button>
       </div>
     )
