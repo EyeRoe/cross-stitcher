2aa89da17f2245a636baaeaee2a0e3259d8d9ed6
EyeRoe <EyeRoe@users.noreply.github.com>
2018-03-29 14:53:31 -0700

    added clear button

--- src/components/SaveButtons.js
+++ src/components/SaveButtons.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import { Button } from 'react-bootstrap'
 
 class SaveButtons extends Component {
-  constructor(props)
-  {
+  constructor(props) {
     super(props);
 
     this.exportGrid = this.exportGrid.bind(this);
@@ -13,25 +12,30 @@ class SaveButtons extends Component {
     let csvVar = this.props.handlerExport()
 
     let csvContent = "data:text/csv;charset=utf-8,";
-    csvVar.forEach(function(rowArray){
+    csvVar.forEach(function (rowArray) {
       let row = rowArray.join(",");
       csvContent += row + "\r\n";
-    }); 
+    });
 
     var csvFile = encodeURI(csvContent);
 
     // Hack for naming files
-		var link = document.createElement('a');
-		link.download = "cross-stitch.csv";
-		link.href = 'data:,' + csvFile;
-		link.click();
+    var link = document.createElement('a');
+    link.download = "cross-stitch.csv";
+    link.href = 'data:,' + csvFile;
+    link.click();
   }
 
-  render () {
+  render() {
     return (
       <div className="App-header">
-        <Button id="magicButton" bsStyle="default" bsSize="large" onClick={ this.exportGrid }>
+        <Button id="magicButton" bsStyle="default" bsSize="large" onClick={this.exportGrid}>
           Export
+        </Button>
+        <Button id="magicButton" bsStyle="default" bsSize="large" onClick={function refreshPage() {
+          window.location.reload();
+        }}>
+          Erase Design
         </Button>
       </div>
     )
